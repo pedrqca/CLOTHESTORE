8e7be2b5deea251e137fbd0f160c34fae09392b7
pedrqca <phcosta@gec.inatel.br>
2026-09-24 09:49:49 -0300

    refactor(frontend): configure api url by environment

--- clothestore-frontend/src/services/api.ts
+++ clothestore-frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3333';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getProducts() {
     const response = await fetch(`${API_URL}/products`);
